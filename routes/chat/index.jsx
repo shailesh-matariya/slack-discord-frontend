1e630fe1aa6c503be523491e549304a857cf772b
Shailesh Matariya <patel.shailesh987@gmail.com>
2022-08-05 19:02:42 +0530

    leftsidebar and conversation

--- routes/chat/index.jsx
+++ routes/chat/index.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material';
 import { Container } from '@mui/system';
 import Channels from '../../core/components/Channels';
 import Conversation from '../../core/components/Conversation';
+import { styled, alpha } from '@mui/material/styles';
 
 const Chat = () => {
   const channels = [{
@@ -16,17 +17,65 @@ const Chat = () => {
     id: 5, name: 'Channel 5'
   }, {
     id: 6, name: 'Channel 6'
-  }];
+  }, {
+    id: 7, name: 'Channel 1'
+  }, {
+    id: 8, name: 'Channel 2'
+  }, {
+    id: 9, name: 'Channel 3'
+  }, {
+    id: 10, name: 'Channel 4'
+  }, {
+    id: 11, name: 'Channel 5'
+  }, {
+    id: 12, name: 'Channel 6'
+  }, {
+    id: 13, name: 'Channel 1'
+  }, {
+    id: 14, name: 'Channel 2'
+  }, {
+    id: 15, name: 'Channel 3'
+  }, {
+    id: 16, name: 'Channel 4'
+  }, {
+    id: 17, name: 'Channel 5'
+  }, {
+    id: 18, name: 'Channel 6'
+  },
+  , {
+    id: 19, name: 'Channel 1'
+  }, {
+    id: 20, name: 'Channel 2'
+  }, {
+    id: 21, name: 'Channel 3'
+  }, {
+    id: 22, name: 'Channel 4'
+  }, {
+    id: 23, name: 'Channel 5'
+  }, {
+    id: 24, name: 'Channel 6'
+  },
+  ];
+
+  const ConversationWrapper = styled('div')(({ theme }) => ({
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  }));
 
   return (
     <Grid container>
-      <Grid item xs={2}>
+      <Grid item xs={2} sx={{ display: { lg: 'flex', xs: 'hidden' } }}>
         <Channels list={channels} />
       </Grid>
 
       <Grid item xs={10}>
         <Container>
-          <Conversation />
+          <ConversationWrapper>
+            <Conversation />
+          </ConversationWrapper>
         </Container>
       </Grid>
     </Grid>
